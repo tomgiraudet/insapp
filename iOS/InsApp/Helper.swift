8b0cc3725a062b28f5b75273956e3b0e886aa262
Tom GIRAUDET <tom.giraudet@insa-rennes.Fr>
2016-02-23 00:53:08 +0100

    Initial commit

---
+++ iOS/InsApp/Helper.swift
@@ -0,0 +1,13 @@
+//
+//  Helper.swift
+//  InsApp
+//
+//  Created by Tom Giraudet on 23/02/16.
+//  Copyright © 2016 Tom Giraudet. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+let generalColor = UIColor(red: 0xEC, green: 0x5D, blue: 0x57, alpha: 1)
---
+++ iOS/InsApp/MeViewController.swift
@@ -0,0 +1,32 @@
+//
+//  FirstViewController.swift
+//  InsApp
+//
+//  Created by Tom Giraudet on 29/01/16.
+//  Copyright © 2016 Tom Giraudet. All rights reserved.
+//
+
+import UIKit
+
+class MeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Navigation Bar
+        let navigationBar = UINavigationBar(frame: CGRectMake(0, 20, self.view.frame.size.width, 44))
+        navigationBar.backgroundColor = UIColor.redColor()
+        let navigationItem = UINavigationItem()
+        navigationItem.title = "Search"
+        navigationBar.items = [navigationItem]
+        self.view.addSubview(navigationBar)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
